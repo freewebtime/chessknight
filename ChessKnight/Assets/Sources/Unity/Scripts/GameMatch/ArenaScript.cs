722c563ed84382df151faf51c4886fcdb2488c5c
Jack Sea <jack.sidorenko@hotmail.com>
2018-05-06 23:25:11 +0300

    Beautifull map concepts

---
+++ ChessKnight/Assets/Sources/Unity/Scripts/GameMatch/ArenaScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ChessKnight.Unity.GameMatch
+{
+    public class ArenaScript: MonoBehaviour
+    {
+        public Transform Background;
+        public Transform GameArea;
+    }
+}
