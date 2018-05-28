607b950a35ff192ab17620ba416f7f8246868668
Jack Sea <jack.sidorenko@hotmail.com>
2018-05-28 05:18:49 +0300

    Generate level blueprint system

---
+++ ChessKnightECS/Assets/GameCode/GameLevel/Data.cs
@@ -0,0 +1,57 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace ChessKnight.GameLevel
+{
+    public struct Level: IComponentData
+    {
+        public int Id;
+        public int2 roomSize;
+        public int2 deskSize;
+        public int2 deskOffset;
+    }
+
+    public enum LevelItemType
+    {
+        Background,
+        DeskCell,
+        CellFigure,
+        PlayerUnit,
+        Star,
+        Lock,
+        Bomb,
+        Block
+    }
+
+    public struct LevelItemBlueprint
+    {
+        public int2 coordinate;
+        public LevelItemType itemType;
+        public int version;
+    }
+
+    public struct LevelBlueprint: ISharedComponentData
+    {
+        public int levelId;
+        public int2 roomSize;
+        public int2 deskSize;
+        public int2 deskOffset;
+        public LevelItemBlueprint[] levelItems;
+    }
+
+    public struct LevelItem: IComponentData
+    {
+        public LevelItemType ItemType;
+        public int Version;
+    }
+
+    public enum ChessFigure
+    {
+        Pawn = 0,
+        Rook = 1,
+        Bishop = 2,
+        Knight = 3,
+        Queen = 4,
+        King = 5
+    }
+}
--- ChessKnightECS/Assets/GameCode/Startup.cs
+++ ChessKnightECS/Assets/GameCode/Startup.cs
@@ -1,11 +1,7 @@
 ﻿using ChessKnight.Application;
-using ChessKnight.GameMatch;
-using ChessKnight.Gameplay;
-using toinfiniityandbeyond.Rendering2D;
+using ChessKnight.GameLevel;
 using Unity.Entities;
 using Unity.Mathematics;
-using Unity.Transforms;
-using Unity.Transforms2D;
 using UnityEngine;
 
 namespace ChessKnight
@@ -24,11 +20,31 @@ namespace ChessKnight
         {
             var entityManager = World.Active.GetOrCreateManager<EntityManager>();
 
-            var loadMatchRequest = entityManager.CreateEntity(ComponentType.Create<LoadMatchRequest>());
-            entityManager.SetComponentData(loadMatchRequest, new LoadMatchRequest {
+            //var loadMatchRequest = entityManager.CreateEntity(ComponentType.Create<LoadMatchRequest>());
+            //entityManager.SetComponentData(loadMatchRequest, new LoadMatchRequest {
+            //    deskSize = new int2(10, 10),
+            //    roomSize = new int2(30, 30)
+            //});
+
+            var generateLevelRequest = new GenerateLevelRequest
+            {
+                roomSize = new int2(30, 20),
                 deskSize = new int2(10, 10),
-                roomSize = new int2(30, 30)
-            });
+                deskOffset = new int2(10, 5),
+
+                isFirstStepAnywhere = 1,
+                isStartingOnDesk = 0,
+
+                seed = (int)(Time.time * Random.value * 1000),
+
+                blockChance = 0.1f,
+                bombChance = 0.2f,
+                starChance = 0.3f,
+                lockChance = 0.4f,
+            };
+
+            var requestEntity = entityManager.CreateEntity(ComponentType.Create<GenerateLevelRequest>());
+            entityManager.SetComponentData(requestEntity, generateLevelRequest);
         }
     }
 }
